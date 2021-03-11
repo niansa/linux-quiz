4726810ad1d112e571f9eb0089190da5fce2fb8b
ODIRF <odirf123@gmail.com>
2021-03-11 20:37:06 +0100

    small fixes, and add a few more questions

--- quiz_de.py
+++ quiz_de.py
@@ -10,18 +10,18 @@ def question(question_title, answer_1, answer_2, answer_3, answer_right):
     question_count = question_count + 1
     print(str(question_count) + ". " + question_title)
     time.sleep(0.2)
-    print("a)" + answer_1 +"\nb)" + answer_2 + "\nc)" + answer_3, end="")
-    answer_user = input(" |  ")
+    print("a) " + answer_1 +"\nb) " + answer_2 + "\nc) " + answer_3, end="")
+    answer_user = input("\n ———> ")
     if answer_user == answer_right:
-        print("Right.")
+        print("Richtig")
         answer_count = answer_count + 1
     else:
-        print("Wrong.")
+        print("Falsch")
     time.sleep(0.6)
     os.system('clear')
      
 # start promt when code runs
-start = input('Willkommen zum Linux Quiz. Du musst jetzt alle Fragen beantworten (mit "a" "b" oder "c"). Um das Quiz zu starten, drücke Enter: ')
+start = input('Willkommen zum Linux Quiz. Du musst jetzt alle Fragen mit "a" "b" oder "c" beantworten. Um das Quiz zu starten, drücke Enter: ')
 
 if start == "":
     print("Loading...")
@@ -31,9 +31,12 @@ if start == "":
 
 # defined all questions, the specific answers, and the correct answer (a, b, or c)
 question("Was ist Linux?", "Eine Desktopumgebung", "Ein Kernel", "Ein Betriebssystem", "b")
-question("Welche Distribution ist die älteste, die noch weiterentwickelt wird?", "Ubuntu", "OpenSuse", "Slackware", "c")
 question("Wie heißt das Maskottchen von Linux?", "Tuz", "Tux", "Tuc", "b")
+question("Welche Distribution ist die älteste, die noch weiterentwickelt wird?", "Ubuntu", "OpenSuse", "Slackware", "c")
 question("Welche Distribution basiert nicht auf Debian?", "Arch Linux", "Raspbian", "Ubuntu", "a")
+question("Seit wann gibt es Linux?", "seit 1991", "seit 1985", "seit 2004", "a")
+question("Unter welcher Lizenz wird Linux herausgegeben?", "Apache 2.0", "Mozilla Public Licence", "GPLv2", "c")
+question("Welche dieser Distribution wird allgemein für Anfänger empfohlen?", "OpenSuse", "Linux Mint", "Kali Linux", "b")
 # ^more questions in progress 
 
 # displays some motivating words and the final result 
@@ -41,9 +44,9 @@ ratio = answer_count / question_count
 if ratio >= 0.8:
     result = "—> Super! Du scheinst dich echt sehr gut mit Linux auszukennen."
 elif ratio >= 0.5:
-    result = "—> Es geht! Hier und dort könntest du noch Wissenslücken füllen. ;)"
+    result = "—> Relativ gut! Hier und dort könntest du aber noch Wissenslücken füllen. ;)"
 else:
-    result = "–> Naja. Informiere dich eventuell noch einmal etwas mehr über Linux, und probiere es anschließend erneut. – Übung macht den Meister!"
+    result = "–> Naja... Informiere dich eventuell noch einmal etwas mehr über Linux, und probiere es anschließend erneut. Denn Übung macht den Meister!"
     
 score = "—————————————–\nDas Quiz ist zu Ende. Dein Ergebnis sind " + str(answer_count*2) + " von " + str(question_count*2) + " erreichbaren Punkten!\n" + result
 print(score)
